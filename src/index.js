0163d0f94440a3d2f89765957374e451b76412df
Lizzie Siegle <esiegle@brynmawr.edu>
2024-08-27 20:44:47 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,55 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { YoutubeTranscript } from "youtube-transcript";
+import html from '../static/index.html';
+
+function* extractSentencesFromTranscript(transcript) {
+	let sentenceParts = [];
+
+	for (let i = 0; i < transcript.length; i++) {
+		const snippet = transcript[i].text;
+		sentenceParts.push(snippet);
+
+		if (snippet.endsWith(".")) {
+			const fullSentence = sentenceParts.join(" ").replace(/\n/g, " ");
+			yield fullSentence;
+			sentenceParts = [];
+		}
+	}
+
+	// Yield any remaining text as the last sentence
+	if (sentenceParts.length > 0) {
+		yield sentenceParts.join(" ").replace(/\n/g, " ");
+	}
+}
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+
+        if (request.method === 'GET' && url.pathname === '/') {
+            return new Response(html, {
+                headers: { 'Content-Type': 'text/html' },
+            });
+        } else if (request.method === 'POST' && url.pathname === '/transcript') {
+			const { url } = await request.json();
+			console.log(`url hurr ${url}`);
+			const transcript = await YoutubeTranscript.fetchTranscript(url); //https://www.youtube.com/watch?v=8RCL5neas_M
+        	const sentences = Array.from(extractSentencesFromTranscript(transcript));
+			console.log(`request.body ${JSON.stringify(request.body)}`)
+      		await env.MY_BUCKET.put(sentences, request.body);
+      		console.log(`Object ${sentences} uploaded successfully!`);
+			
+			const messages = [
+				{ role: "system", content: "You are a friendly assistant" },
+				{
+				  role: "user",
+				  content: `Summarize the given YouTube video transcript: ${sentences}`,
+				},
+			];
+			const response = await env.AI.run("@cf/meta/llama-3.1-8b-instruct", { messages });
+			console.log(`response ${JSON.stringify(response)}`);
+			return new Response(JSON.stringify(response), {
+				headers: { 'Content-Type': 'application/json' },
+			});
+		}
 	},
 };
